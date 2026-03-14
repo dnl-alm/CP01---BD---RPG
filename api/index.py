0924bf3a79386cb71dec9ee0108eba7db53043f9
dnl-alm <danielfonseca.almeida@gmail.com>
2026-03-14 00:27:05 -0300

    agora vai

--- api/index.py
+++ api/index.py
@@ -1,16 +1,6 @@
-from flask import Flask, jsonify
-import banco
+import sys
+import os
 
-app = Flask(__name__)
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
-@app.route("/herois", methods=['GET'])
-def get_herois():
-    herois = banco.listar_herois()
-    return {"herois": herois}, 200
-
-@app.route("/dano-nevoa", methods=["PUT"])
-def processar():
-    banco.dano_nevoa()
-    return jsonify({"msg": "Turno processado"})
-
-handler = app
+from app import app
