bbc1c714c310c51aea1a5a7d2d57b10e3e622de2
dnl-alm <danielfonseca.almeida@gmail.com>
2026-03-14 00:09:54 -0300

    reformulado para o vercel

--- api/index.py
+++ api/index.py
@@ -1,1 +1,16 @@
-from app import app
+from flask import Flask, jsonify
+import banco
+
+app = Flask(__name__)
+
+@app.route("/herois", methods=['GET'])
+def get_herois():
+    herois = banco.listar_herois()
+    return {"herois": herois}, 200
+
+@app.route("/dano-nevoa", methods=["PUT"])
+def processar():
+    banco.dano_nevoa()
+    return jsonify({"msg": "Turno processado"})
+
+handler = app
